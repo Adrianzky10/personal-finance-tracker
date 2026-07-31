ec26352ffb0118a4f649014c75418fa194d898fe
Adrianzky10 <adriandwii14022@gmail.com>
2026-07-31 12:29:42 +0800

    feat(auth): setup QueryClientProviders

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import type { ReactNode } from "react";
 import "./globals.css";
+import { QueryProviders } from "@/providers/QueryProviders";
 
 export const metadata: Metadata = {
   title: {
@@ -17,7 +18,9 @@ type RootLayoutProps = Readonly<{
 export default function RootLayout({ children }: RootLayoutProps) {
   return (
     <html lang="id">
-      <body>{children}</body>
+      <body className="bg-app-background text-text-primary antialiased">
+        <QueryProviders>{children}</QueryProviders>
+      </body>
     </html>
   );
 }
